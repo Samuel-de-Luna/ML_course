9ac91a897508407c0bf82e1080db02967462fb85
Samuel De Luna <smadeluna@gmail.com>
2025-12-05 11:14:32 +0100

    10

--- labs/ex10/template/helper.py
+++ labs/ex10/template/helper.py
@@ -41,9 +41,14 @@ def build_distance_matrix(data, mu):
         data: numpy array of shape = (N, d). original data.
         mu:   numpy array of shape = (k, d). Each row corresponds to a cluster center.
     Returns:
-        squared distances matrix,  numpy array of shape (N, k):
-            row number i column j corresponds to the squared distance of datapoint i with cluster center j.
+        numpy array of shape (N, k):
+            squared distances matrix,
+            the value row i column j corresponds to the squared distance of datapoint i with cluster center j.
     """
-    ####################################
-    ### ___ Enter your code here ___ ###
-    ####################################
+    
+    N = data.shape[0]
+    k = mu.shape[0]
+    dist_matrix = np.zeros((N, k))
+    for i in range(k):
+        dist_matrix[:, i] = np.sum((data - mu[i]) ** 2, axis=1)
+    return dist_matrix
